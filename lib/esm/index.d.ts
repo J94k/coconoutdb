0767505d2bef539add2bfe8a6eae332859f9a576
NotEternal <cent1pede@protonmail.ch>
2022-05-29 16:19:20 +0300

    Remove a lib directory

--- lib/esm/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as Store } from './Store';
-export * from './Store';
-export { observer } from 'mobx-react-lite';
--- lib/esm/utils/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function log({ title, color, value }: {
-    title: string;
-    value: any;
-    color?: string;
-}): void;
